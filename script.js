e0b0c6af195a884a4a0eb6fef65f06b534a8487d
Marta <maeldeis@gmail.com>
2024-01-26 20:18:25 +0100

    Begun the JS with addition of API key and variables start point

---
+++ script.js
@@ -0,0 +1,7 @@
+const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+const weatherApiKey = "123456789";
+
+var cityEl = $(".city");
+var dateEl = $(".date");
+var tempEl = $(".temp");
+
