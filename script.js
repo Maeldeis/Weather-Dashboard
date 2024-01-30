134d7090a9f4c1d585b7c2bdd0c15bf47c61913f
Marta <maeldeis@gmail.com>
2024-01-30 22:25:21 +0100

    javascript vars claimed and added forloop to persist data into html page

--- script.js
+++
@@ -1,7 +0,0 @@
-const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
-const weatherApiKey = "123456789";
-
-var cityEl = $(".city");
-var dateEl = $(".date");
-var tempEl = $(".temp");
-
